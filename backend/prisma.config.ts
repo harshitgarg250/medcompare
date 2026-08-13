2ffdf619def003eba8f81400655f2bc06db572b6
Harshit Garg <harshitgarg674@gmail.com>
2026-08-13 16:23:10 +0530

    fix bug

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,8 +1,12 @@
+/// <reference types="node" />
+
 import 'dotenv/config'
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  datasource: {
-    url: process.env.DIRECT_URL,
+  datasources: {
+    db: {
+      url: process.env.DIRECT_URL!,
+    },
   },
 })
